7b58882160fe94e3f3984d45bee8d9ec001e917c
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-07-25 13:58:16 -0500

    can load all boards and assign them a boardid

--- app/controllers/boardsCtrl.js
+++ app/controllers/boardsCtrl.js
@@ -1,8 +1,11 @@
+"use strict";
 app.controller('BoardsCtrl', function($scope, DataFactory, AuthFactory) {
 
 	DataFactory.loadBoardsByUser(AuthFactory.getUser()).
-	then(function(stuff){
-		console.log(stuff);
-	})
+	then(function(boards){
+		console.log(boards);
+	});
 
-});
+		});
+
+
--- app/factories/DataFactory.js
+++ app/factories/DataFactory.js
@@ -3,10 +3,17 @@
 app.factory("DataFactory", function( FirebaseCreds, $q, $http){
 
 	let loadBoardsByUser = function (uid){
+		let boards=[];
 		return $q(function(resolve,reject){
 			$http.get(`${FirebaseCreds.databaseURL}/boards.json?orderBy="uid"&equalTo="${uid}"`).
 			success(function(snapshot){
-				resolve(snapshot);
+				if(snapshot !== null){
+				Object.keys(snapshot).forEach(function(key){
+					snapshot[key].boardid=key;
+					boards.push(snapshot[key]);
+				});
+			}
+				resolve(boards);
 			}).error(function(error){
 				reject(error);
 			});
