6d2b86e063767b986b8495601e5e2f369a3d7757
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-07-25 14:51:28 -0500

    Added modals that support injecting templates

--- app/controllers/boardsCtrl.js
+++ app/controllers/boardsCtrl.js
@@ -1,11 +1,12 @@
 "use strict";
-app.controller('BoardsCtrl', function($scope, DataFactory, AuthFactory) {
+app.controller('BoardsCtrl', function($scope, DataFactory, AuthFactory, boardModal) {
 
 	DataFactory.loadBoardsByUser(AuthFactory.getUser()).
 	then(function(boards){
 		console.log(boards);
 	});
-
-		});
+		$scope.openModal = boardModal.activate;
+	
+});
 
 
