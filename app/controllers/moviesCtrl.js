c3e7a6bcc828d6bd94dc7e53c6a5d7482fc20fe8
Dave Harms <daveharmswebdev@gmail.com>
2016-07-27 07:38:26 -0500

    got pinmodal to activate when movie is selected, need to pass movie info to pins collection in firebase

--- app/controllers/moviesCtrl.js
+++ app/controllers/moviesCtrl.js
@@ -1,11 +1,15 @@
 'use strict';
 
-app.controller('moviesCtrl', function($scope, MoviesF) {
+app.controller('moviesCtrl', function($scope, MoviesF, ItemToPin, PinModal) {
   // search object to passed to MovieFactory
 
   $scope.movies = MoviesF.getMovies();
   console.log($scope.movies);
   $scope.selectMovie = function(movie) {
+    ItemToPin.setItem(movie);
     console.log(movie);
+    PinModal.activate();
   };
+
+  $scope.openPinModal = PinModal.activate;
 });
